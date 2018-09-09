58cbbba3c420449f12507e971e7b870dd1094330
Gun Gun Priatna <gungunpriatna02@gmail.com>
2018-09-09 18:21:14 +0700

    commit pertama :tada:

---
+++ db/foto.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `foto` (
+ `id` int(11) NOT NULL AUTO_INCREMENT,
+ `nama` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+ PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
